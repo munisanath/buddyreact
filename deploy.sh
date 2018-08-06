7e20e81d40cad281ed8a6aeae58f434f36e4eab9
munisanath <shodamgula.sanath@gmail.com>
2018-08-06 18:55:40 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
+#/bin/bash
 #ssh-keygen -t  rsa -b 4096 -C 'nodejs-webapp@travis-ci.org' -f ./deploy_rsa
 
 #ssh-copy-id -i deploy_rsa.pub azureadmin@104.211.299.97
-echo "create this directory in azure"
-mkdir test
+function run_commands () {
+	echo "login into github"
+	git clone https://github.com/munisanath/nodejs-webapp.git
+	cd nodejs-webapp
+	docker build -t nodejs-webapp deployment/.
+	cd nodejs-webapp/deployment
+	docker-compose up -d
+}
+sshpass -p "Suventure@2012$" ssh azureadmin@104.211.299.97 "$(typeset -f run_commands); run_commands"
