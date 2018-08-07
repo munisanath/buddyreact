97999f9c25bb5e1934af19fedd189e0ac6786be2
munisanath <shodamgula.sanath@gmail.com>
2018-08-07 19:13:20 +0530

    added

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 #ssh-keygen -t  rsa -b 4096 -C 'nodejs-webapp@travis-ci.org' -f ./deploy_rsa
 
 #ssh-copy-id -i deploy_rsa.pub azureadmin@104.211.299.97
+set -x
 echo script
 function run_commands () {
 	echo "login into github"
@@ -11,4 +12,4 @@ function run_commands () {
 	cd deployment
 	docker-compose up -d
 }
-sshpass -p "Suventure@2012$" ssh azureadmin@itasset.southindia.cloudapp.azure.com "$(typeset -f run_commands); run_commands"
+sshpass -p "Suventure@2012$" ssh -o StrictHostKeyChecking=no azureadmin@itasset.southindia.cloudapp.azure.com "$(typeset -f run_commands); run_commands"
