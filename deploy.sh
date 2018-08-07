7d454d7883ff1d0c962195ca3a747b18f450ad25
munisanath <shodamgula.sanath@gmail.com>
2018-08-07 21:02:09 +0530

    script changes

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,17 @@ function run_commands () {
 	echo "login into github"
 	rm -rf nodejs-webapp
 	#directory check
-	function die() {
-	echo "$@" 1>&2
-	exit 1
-	}
+	# function die() {
+	# echo "$@" 1>&2
+	# exit 1
+	# }
 	pwd=`pwd`
 	folder="${pwd}/volume"
 
 	if [[ ! -d "$folder" ]] && [[ ! -f "$folder" ]]; then
 		mkdir -p "$folder"
 	else
-		die "$folder already exists but is not a directory" 1>&2
+		die "$folder already exists but is not a directory"
 	fi
 	git clone https://github.com/munisanath/nodejs-webapp.git
 	cd nodejs-webapp
