314f3442ae3869f1071b56d32d9e20d0f5834fde
munisanath <shodamgula.sanath@gmail.com>
2018-08-07 18:37:51 +0530

    script commented

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ function run_commands () {
 	echo "login into github"
 	git clone https://github.com/munisanath/nodejs-webapp.git
 	cd nodejs-webapp
-	docker build -t nodejs-webapp deployment/.
-	cd nodejs-webapp/deployment
-	docker-compose up -d
+	# docker build -t nodejs-webapp deployment/.
+	# cd nodejs-webapp/deployment
+	# docker-compose up -d
 }
 sshpass -p "Suventure@2012$" ssh azureadmin@itasset.southindia.cloudapp.azure.com "$(typeset -f run_commands); run_commands"
